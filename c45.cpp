8b74fdf154d1436345c8c19716b9b31c9619d100
jidezhuwithyou <6536511+jidezhuwithyou@user.noreply.gitee.com>
2021-11-09 16:01:44 +0800

    add a file

---
+++ c45.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a[5][3] = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9},
+        {1,2,3},
+        {3,4,5}
+    };
+    cout << "********"<< a[1] <<endl;
+}
