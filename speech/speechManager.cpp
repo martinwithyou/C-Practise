6b65e68c65240e188ff58adced57e72e9bfc9c3a
jidezhuwithyou <6536511+jidezhuwithyou@user.noreply.gitee.com>
2021-12-13 14:30:04 +0800

    add a file

---
+++ speech/speechManager.cpp
@@ -0,0 +1,19 @@
+#include "speechManager.h"
+
+SpeechManager::SpeechManager(){
+
+}
+
+SpeechManager::~SpeechManager(){
+    
+}
+void SpeechManager::show_Menu(){
+    cout << "*******************" << endl;
+    cout << "*******welcome************" << endl;
+    cout << "*******1.start************" << endl;
+    cout << "*******2.show record************" << endl;
+    cout << "*******3.clear************" << endl;
+    cout << "*******0.exit************" << endl;
+    cout << "*******************" << endl;
+    cout << endl;
+}
---
+++ speech/speechManager.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class SpeechManager{
+    public:
+        //
+        SpeechManager();
+        //
+        ~SpeechManager();
+        //
+        void show_Menu();
+};
