eca69e692139b907805543a5020469d8dc0d0130
jidezhuwithyou <6536511+jidezhuwithyou@user.noreply.gitee.com>
2021-12-13 14:34:44 +0800

    add a file

---
+++ speech/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#include "speechManager.h"
+
+int main()
+{
+    //show menu
+    SpeechManager sm;
+    sm.show_Menu();
+
+
+    system("pause");
+    return 0;
+
+}
